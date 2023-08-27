70c3d403ee9e8c83a232617e6be05862cbddec34
yejin4117 <s2108@e-mirim.hs.kr>
2023-08-27 20:13:55 +0900

    node로 db연결해서 테이블 만들고 데이터 넣기

--- src/root/dbconfig.js
+++ src/root/dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user          :"YJDB",
+    user          : "YJDB",
     password      : "8888",
-    connectString : "192.168.219.105",
+    connectString : "192.168.219.105:1521/ORCL",
     externalAuth  : false
 };
--- src/root/server.js
+++ src/root/server.js
@@ -1,44 +1,75 @@
-const express = require('express');
-const app = express();
-const oracledb = require('oracledb');
-const dbConfig = require('./dbconfig.js');
+var oracledb = require("oracledb");
+var dbConfig = require("./dbConfig.js");
 
-async function connectToDB() {
+oracledb.getConnection({
+  user: dbConfig.user,
+  password: dbConfig.password,
+  connectString: dbConfig.connectString,
+})
+.then(async (conn) => {
+  console.log("Oracle DB 연결 성공!!");
+
   try {
-    await oracledb.createPool({
-      user: dbConfig.user,
-      password: dbConfig.password,
-      connectString: dbConfig.connectString,
-    });
-    console.log('Connected to Oracle DB');
+    // Create ART table
+    let sql = `CREATE TABLE ART (
+      ARTID VARCHAR2(50),
+      TITLE VARCHAR2(50),
+      ARTIST VARCHAR2(50)
+    )`;
+    await conn.execute(sql);
+
+    sql = "ALTER TABLE art ADD PRIMARY KEY(artid)";
+    await conn.execute(sql);
+
+    // Create MYUSER table
+    sql = `CREATE TABLE MYUSER (
+      ID VARCHAR2(20),
+      PASSWORD VARCHAR2(20),
+      NAME VARCHAR2(50),
+      BIRTHDAY DATE
+    )`;
+    await conn.execute(sql);
+
+    sql = "ALTER TABLE myuser ADD PRIMARY KEY(id)";
+    await conn.execute(sql);
+
+    // Create COMMENTS table
+    sql = `CREATE TABLE COMMENTS (
+      COMID VARCHAR2(50),
+      ID VARCHAR2(50),
+      ARTID VARCHAR2(50),
+      COMCONTENTS VARCHAR2(50)
+    )`;
+    await conn.execute(sql);
+
+    sql = "ALTER TABLE comments ADD PRIMARY KEY(comid)";
+    await conn.execute(sql);
+
+    sql = `ALTER TABLE comments ADD FOREIGN KEY(id) REFERENCES myuser(id)`;
+    await conn.execute(sql);
+
+    sql = `ALTER TABLE comments ADD FOREIGN KEY(artid) REFERENCES art(artid)`;
+    await conn.execute(sql);
+
+    // Create SEARCH table
+    sql = `CREATE TABLE SEARCH (
+      SEARID VARCHAR2(100),
+      SEARCONTENTS VARCHAR2(100)
+    )`;
+    await conn.execute(sql);
+
+    sql = "ALTER TABLE search ADD PRIMARY KEY(searid)";
+    await conn.execute(sql);
+
+    console.log("테이블 생성 및 관계 설정 완료!!");
   } catch (err) {
-    console.error('Error connecting to Oracle DB:', err);
+    console.error("에러:", err);
+  } finally {
+    if (conn) {
+      await conn.close();
+    }
   }
-}
-
-app.get('/', (req, res) => {
-  res.send('Hello, this is the root!');
+})
+.catch((err) => {
+  console.error("연결 에러:", err);
 });
-
-app.get('/api/data', async (req, res) => {
-  try {
-    const connection = await oracledb.getConnection();
-    const result = await connection.execute('SELECT * FROM myuser');
-    await connection.close();
-
-    res.json(result.rows);
-  } catch (err) {
-    console.error('Error executing Oracle query:', err);
-    res.status(500).json({ error: err.message }); // 실제 오류 메시지를 반환
-  }
-});
-
-const PORT = 5000;
-(async () => {
-  await connectToDB(); // 데이터베이스 연결 완료 후 앱 시작
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-})();
-
-connectToDB();
